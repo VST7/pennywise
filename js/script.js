ebd82851be3b137e705129113b1bf0db4ff23302
Vaibhav <vaibhav710thakur@gmail.com>
2021-01-25 19:35:25 +0530

    pennywise

---
+++ js/script.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function ($) {
+	//     setTimeout(function() {
+	//       $('#chat-bubble').fadeOut('fast');
+	//   }, 5000);
+
+	AOS.init();
+});
+
+// this is the code for pop up
+$(document).ready(function () {
+	//Fade in delay for the background overlay (control timing here)
+	$("#bkgOverlay").delay(4800).fadeIn(400);
+	//Fade in delay for the popup (control timing here)
+	$("#delayedPopup").delay(5000).fadeIn(400);
+
+	//Hide dialouge and background when the user clicks the close button
+	$("#btnClose").click(function (e) {
+		HideDialog();
+		e.preventDefault();
+	});
+});
+//Controls how the modal popup is closed with the close button
+function HideDialog() {
+	$("#bkgOverlay").fadeOut(400);
+	$("#delayedPopup").fadeOut(300);
+}
